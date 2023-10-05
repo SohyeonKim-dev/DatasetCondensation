2ca798e905548fe10ac50b4b094abe540b8269ee
SoHyeon Kim <happythgus89@gmail.com>
2023-10-05 15:18:04 +0900

    DC with GM

---
+++ DC with Gradient Matching/Original_DC_with_GM/main.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+#SBATCH -J test-run
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-gpu=8
+#SBATCH --mem-per-gpu=29G
+#SBATCH -p batch_ce_ugrad
+#SBATCH -w moana-r1
+#SBATCH -t 1-0
+#SBATCH -o logs/slurm-%A.out
+
+pwd
+which python
+hostname
+python main.py
+
+exit 0
+
